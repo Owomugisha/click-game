6ba62796d914953e0add1bfc8926861ddb99f5da
Owomugisha Birungi <brinnetbirungi@yahoo.com>
2019-04-11 23:21:15 -0400

    Updates

---
+++ precache-manifest.a784dd71593f75467311db8793b4e686.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aa765b97c1e7aaa53c3e",
+    "url": "/click-game/static/js/runtime~main.7d043b49.js"
+  },
+  {
+    "revision": "3df318d1186f5241b1b3",
+    "url": "/click-game/static/js/main.98ee8e53.chunk.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/click-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "3df318d1186f5241b1b3",
+    "url": "/click-game/static/css/main.7862997a.chunk.css"
+  },
+  {
+    "revision": "04a64b7238ef13ddcf968a030a3cf0c5",
+    "url": "/click-game/index.html"
+  }
+];
